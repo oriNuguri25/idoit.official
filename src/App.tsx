6d36a4234128ec53a04024b806013715f0ac703e
oriNuguri25 <byeongjun25@naver.com>
2025-05-27 22:49:44 +0900

    Toast 상위 폴더에 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import AppRoutes from "./routes/AppRoutes";
 import { AuthProvider } from "@/context/AuthContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "./components/ui/sonner";
 
 const queryClient = new QueryClient();
 
@@ -9,6 +10,7 @@ const App = () => {
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <AppRoutes />
+        <Toaster />
       </AuthProvider>
     </QueryClientProvider>
   );
